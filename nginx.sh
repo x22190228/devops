333d8eae76c0e0d1e8fda1b23b2754e976946178
x22190228 <152830376+x22190228@users.noreply.github.com>
2023-12-10 22:03:43 +0000

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo cp -rf app.conf /etc/nginx/sites-available/app
-chmod 710 /var/lib/jenkins/workspace/django-cicd
+chmod 710 /var/lib/jenkins/workspace/devops-cicd
 
 sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled
 sudo nginx -t
@@ -9,6 +9,6 @@ sudo nginx -t
 sudo systemctl start nginx
 sudo systemctl enable nginx
 
-echo "Nginx has been started"
+echo "Nginx started"
 
 sudo systemctl status nginx

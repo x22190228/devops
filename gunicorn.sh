a8ccb0fdca96270e180ae8175a978cedd5de7050
x22190228 <152830376+x22190228@users.noreply.github.com>
2023-12-10 22:24:32 +0000

    Update gunicorn.sh

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 cd /var/lib/jenkins/workspace/devops-cicd/env/bin/python3
 source env/bin/activate
-
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py runserver
 
 
 

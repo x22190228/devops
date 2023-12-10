20091ab9645db334af6724b1fe0b6d7d03957fc9
x22190228 <152830376+x22190228@users.noreply.github.com>
2023-12-10 22:40:45 +0000

    Update gunicorn.sh

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,10 +1,36 @@
-#!/bin/bash
-cd /var/lib/jenkins/workspace/devops-cicd
+# #!/bin/bash
+
 source env/bin/activate
-cd app
+
+cd /var/lib/jenkins/workspace/django-cicd/app
+
 python3 manage.py makemigrations
 python3 manage.py migrate
-python3 manage.py runserver
+python3 manage.py collectstatic -- no-input
+
+echo "Migrations done"
+
+cd /var/lib/jenkins/workspace/django-cicd
+
+sudo cp -rf gunicorn.socket /etc/systemd/system/
+sudo cp -rf gunicorn.service /etc/systemd/system/
+
+echo "$USER"
+echo "$PWD"
 
 
+
+sudo systemctl daemon-reload
+sudo systemctl start gunicorn
+
+echo "Gunicorn has started."
+
+sudo systemctl enable gunicorn
+
+echo "Gunicorn has been enabled."
+
+sudo systemctl restart gunicorn
+
+
+sudo systemctl status gunicorn
 

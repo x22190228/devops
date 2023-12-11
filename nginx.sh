a2e9fc0aa9992e5ac2359e6f6f927af8af93da65
x22190228 <152830376+x22190228@users.noreply.github.com>
2023-12-11 00:38:35 +0000

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo cp -rf app.conf /etc/nginx/sites-available/app.conf
+sudo cp -rf app.conf /etc/nginx/conf.d
 chmod 710 /var/lib/jenkins/workspace/devops-cicd
 
-sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled
+# sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled
 sudo nginx -t
 
 sudo systemctl start nginx

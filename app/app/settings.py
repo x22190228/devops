d2ce0c9d6d59e4d2ef240bc6cdd41c7b9fb6afe5
Muhammad Muhteshim Ghazali <x22190228@student.ncirl.ie>
2023-12-12 01:28:58 +0000

    Fixed Allowed host

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-lkewu&k4@+-8^*%)gataq@)t9k3a_6optt(nf8)lagzxrn)3+c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

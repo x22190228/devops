e47f544fd6657083c2828e18ed684f9ecd25125b
Muhammad Muhteshim Ghazali <x22190228@student.ncirl.ie>
2023-12-12 02:46:42 +0000

    delete

--- djangogunicorn.sh
+++ djangogunicorn.sh
@@ -7,29 +7,13 @@ echo "in app folder"
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic -- no-input
-
-echo "Migrations done"
-
 cd /var/lib/jenkins/workspace/devops-cicd
-
 sudo cp -rf gunicorn.socket /etc/systemd/system/
 sudo cp -rf gunicorn.service /etc/systemd/system/
-
-echo "$USER"
-echo "$PWD"
-
-
-
 sudo systemctl daemon-reload
 sudo systemctl start gunicorn
-
 echo "Gunicorn has started."
-
 sudo systemctl enable gunicorn
-
 echo "Gunicorn has been enabled."
-
 sudo systemctl restart gunicorn
-
-
 sudo systemctl status gunicorn
